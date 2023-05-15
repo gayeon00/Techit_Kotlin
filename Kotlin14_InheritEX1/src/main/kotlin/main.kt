29981656a2a6061594e79c41359ad5e135502a95
gayeon00 <chlrkdus4819@gmail.com>
2023-05-15 13:36:20 +0900

    boolean으로 받도록 수정 함수 구조 수정

--- Kotlin14_InheritEX1/src/main/kotlin/main.kt
+++ Kotlin14_InheritEX1/src/main/kotlin/main.kt
@@ -3,17 +3,7 @@ import java.util.*
 fun main() {
     val restaurant = Restaurant()
 
-    var foodType = 0
-
-    while (true) {
-        foodType = restaurant.selectFoodType()
-        if (foodType == 4) {
-            println("종료합니다")
-            break
-        }
-
-        restaurant.addFood(foodType)
-    }
+    restaurant.selectMenu()
 
     restaurant.printStatus()
     restaurant.printFoodList()
@@ -27,27 +17,40 @@ open class Food(var name: String, var price: Int) {
     }
 }
 
-class Jjajang(var isDouble: String) : Food("짜장면", 6000) {
+class Jjajang(var isDouble: Boolean) : Food("짜장면", 6000) {
 
     fun printJjajangInfo() {
         printFoodInfo()
-        println("곱배기여부 : $isDouble")
+        if (isDouble) {
+            println("곱배기여부 : 곱배기 입니다")
+        } else {
+            println("곱배기여부 : 곱배기 아입니다")
+        }
     }
 }
 
-class Jjambbong(var hasMussel: String) : Food("짬뽕", 8000) {
+class Jjambbong(var hasMussel: Boolean) : Food("짬뽕", 8000) {
 
     fun printJjambbongInfo() {
         printFoodInfo()
-        println("홍합여부 : $hasMussel")
+        if (hasMussel) {
+            println("홍합여부 : 홍합이 있습니다")
+        } else {
+            println("홍합여부 : 홍합이 없습니다")
+        }
     }
 
 }
 
-class Fried(var soup: String) : Food("볶음밥", 10000) {
+class Fried(var soup: Boolean) : Food("볶음밥", 10000) {
+
     fun printFriedInfo() {
         printFoodInfo()
-        println("국물종류 : $soup")
+        if (soup) {
+            println("국물종류 : 계란국물")
+        } else {
+            println("국물종류 : 짬뽕국물")
+        }
     }
 }
 
@@ -57,27 +60,39 @@ class Restaurant {
     val jjambbongList = arrayListOf<Jjambbong>()
     val friedList = arrayListOf<Fried>()
 
-    fun selectFoodType(): Int {
-        println("메뉴를 선택해주세요")
-        print("1. 짜장면, 2. 짬뽕, 3. 볶음밥, 4. 종료 : ")
-        return sc.nextInt()
-    }
+    fun selectMenu() {
+        var type = 0
 
-    fun addFood(food: Int) {
-        when (food) {
-            1 -> {
-                print("곱배기 여부를 선택해주세요 : ")
-                jjajangList.add(Jjajang(sc.next()))
+        while (true) {
+
+            println("메뉴를 선택해주세요")
+            print("1. 짜장면, 2. 짬뽕, 3. 볶음밥, 4. 종료 : ")
+            type = sc.nextInt()
+
+            if (type !in 1..4) continue
+
+            if (type == 4) {
+                println("종료합니다")
+                break
             }
-            2 -> {
-                print("홍합 여부를 선택해주세요 : ")
-                jjambbongList.add(Jjambbong(sc.next()))
-            }
-            3 -> {
-                print("국물 종류를 선택해주세요 : ")
-                friedList.add((Fried(sc.next())))
+
+            when (type) {
+                1 -> {
+                    print("곱배기 여부를 선택해주세요 : ")
+                    jjajangList.add(Jjajang(sc.nextBoolean()))
+                }
+                2 -> {
+                    print("홍합 여부를 선택해주세요 : ")
+                    jjambbongList.add(Jjambbong(sc.nextBoolean()))
+                }
+                3 -> {
+                    print("국물 종류를 선택해주세요 : ")
+                    friedList.add((Fried(sc.nextBoolean())))
+                }
             }
         }
+
+
     }
 
     fun calculateTotalPrice(): Int {
